d3e1615800d97a29021ab078f1510a5a7ff50cd6
Pfiffel <pfiffel@googlemail.com>
2022-05-31 04:22:12 +0200

    incr build

--- nav.js
+++ nav.js
@@ -1,4 +1,4 @@
-const BUILD = 25;
+const BUILD = 27;
 const pages = {"enemies":true, "players":true, "symbiotes":true, "items":true, "maps":true, "diff":true};
 const here = getCurrentPage();
 const nav = document.getElementById("nav");
