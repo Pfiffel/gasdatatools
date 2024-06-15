31b276c4d37ae589fa57442085af5ea98d302bd7
Pfiffel <pfiffel@googlemail.com>
2024-06-15 18:03:37 +0200

    build 60 data update

--- nav.js
+++ nav.js
@@ -1,4 +1,4 @@
-const BUILD = 59;
+const BUILD = 60;
 const pages = { "enemies": true, "players": true, "symbiotes": true, "items": true, "maps": true, "diff": true };
 const here = getCurrentPage();
 const nav = document.getElementById("nav");
