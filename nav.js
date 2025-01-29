148241afd4440c232d49acb40e742aa378ce9275
Pfiffel <pfiffel@googlemail.com>
2025-01-29 22:11:15 +0100

    build 74 data update proper

--- nav.js
+++ nav.js
@@ -1,4 +1,4 @@
-const BUILD = 73;
+const BUILD = 74;
 const pages = { "glossary": true, "enemies": true, "players": true, "symbiotes": true, "items": true, "damageTypes": true, "maps": true, "diff": true };
 const here = getCurrentPage();
 const nav = document.getElementById("nav");
